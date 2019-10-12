a19110319a8fb75e86743a5b3929a7483345618b
yonghua4413 <254274509@qq.com>
2019-10-12 18:20:04 +0800

    删除

---
+++ public/js/my/content/list.js
@@ -0,0 +1,77 @@
+$(function () {
+    $('.preview').on("click", function () {
+        var id = $(this).attr('data-id');
+        layer.open({
+            'type':2,
+            'title':"预览",
+            'area':['100%', '100%'],
+            'content':"/content/preview?id="+id
+        });
+    });
+
+    $('.edit').on("click", function () {
+        var id = $(this).attr('data-id');
+        layer.open({
+            'type':2,
+            'title':"预览",
+            'area':['100%', '100%'],
+            'content':"/content/edit?id="+id
+        });
+    });
+
+    $('.del').on("click", function () {
+        var id = $(this).attr('data-id');
+        var title = $(this).attr('data-title');
+        layer.alert("请确认要删除 <span style='color:red;'>" + title + "</span> 吗？",
+            {'title':'警告'}, function () {
+                var _data = {'_token':_token, 'id':id};
+                $.post("/content/del", _data, function (data) {
+                    if(data.status == 0){
+                        layer.msg(data.message, function () {
+                            window.location.reload();
+                        });
+                        return;
+                    }
+                    layer.alert(data.message);
+                });
+            }
+        );
+    });
+
+    $('.add').on('click', function (e) {
+        if (e && e.preventDefault) {
+            e.preventDefault();
+            //IE中阻止函数器默认动作的方式
+        } else {
+            window.event.returnValue = false;
+            return false;
+        }
+        layer.open({
+            'type':2,
+            'title':"发布内容",
+            'area':['100%', '100%'],
+            'content':"/content/add"
+        });
+    });
+
+    layer.photos({
+        photos: '#layer-photos'
+        ,anim: 5 //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
+    });
+});
+
+$('.add-content').on("click", function (e) {
+    if (e && e.preventDefault) {
+        e.preventDefault();
+        //IE中阻止函数器默认动作的方式
+    } else {
+        window.event.returnValue = false;
+        return false;
+    }
+    layer.open({
+        'title':"发布内容",
+        'type':2,
+        'area':['100%', '100%'],
+        'content':"/content/add"
+    });
+});
--- resources/views/content/list.blade.php
+++ resources/views/content/list.blade.php
@@ -24,10 +24,7 @@
 
 
 @section('main')
-
     <div class="wrapper wrapper-content animated fadeInRight ecommerce">
-
-
         <div class="ibox-content m-b-sm border-bottom">
             <div class="row">
                 <form>
@@ -105,7 +102,7 @@
                                         <button class="btn btn-sm btn-info edit" data-id="{{$item->id}}">
                                             <i class="fa fa-edit"></i> 编辑
                                         </button>
-                                        <button class="btn btn-sm btn-danger">
+                                        <button class="btn btn-sm btn-danger del" data-title="{{$item->title}}" data-id="{{$item->id}}">
                                             <i class="fa fa-times"></i> 删除
                                         </button>
                                     </td>
@@ -131,70 +128,6 @@
 @endsection
 
 @section('pageJs')
-    <script>
-        $(function () {
-
-            $('.preview').on("click", function () {
-                var id = $(this).attr('data-id');
-                layer.open({
-                    'type':2,
-                    'title':"预览",
-                    'area':['100%', '100%'],
-                    'content':"/content/preview?id="+id
-                });
-            });
-
-            $('.edit').on("click", function () {
-                var id = $(this).attr('data-id');
-                layer.open({
-                    'type':2,
-                    'title':"预览",
-                    'area':['100%', '100%'],
-                    'content':"/content/edit?id="+id
-                });
-            });
-
-            $('.del').on("click", function () {
-                var id = $(this).attr('data-id');
-                layer.alert("请确认要删除吗？")
-            });
-
-            $('.add').on('click', function (e) {
-                if (e && e.preventDefault) {
-                    e.preventDefault();
-                    //IE中阻止函数器默认动作的方式
-                } else {
-                    window.event.returnValue = false;
-                    return false;
-                }
-                layer.open({
-                    'type':2,
-                    'title':"发布内容",
-                    'area':['100%', '100%'],
-                    'content':"/content/add"
-                });
-            });
-
-            layer.photos({
-                photos: '#layer-photos'
-                ,anim: 5 //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
-            });
-        });
-
-        $('.add-content').on("click", function (e) {
-            if (e && e.preventDefault) {
-                e.preventDefault();
-                //IE中阻止函数器默认动作的方式
-            } else {
-                window.event.returnValue = false;
-                return false;
-            }
-            layer.open({
-                'title':"发布内容",
-                'type':2,
-                'area':['100%', '100%'],
-                'content':"/content/add"
-            });
-        });
-    </script>
+    <script>var _token= '{{csrf_token()}}';</script>
+    <script src="/js/my/content/list.js"></script>
 @endsection
